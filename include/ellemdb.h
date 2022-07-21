04370e8e3b4dedfbd5d47b29a9fc51acb4732d80
Kevin Marschke <kmarschke@ellem.ai>
2022-07-20 22:01:26 -0500

    solved the problems lined out in last commit
    only touched the header file. Also take a look at edb_host_t... I discovered a lot of insight there.

--- include/ellemdb.h
+++ include/ellemdb.h
@@ -15,6 +15,7 @@ typedef edb_oid uint64_t;
 // internal structures
 typedef struct edbh_st edbh;
 typedef uint64_t edb_pid;
+typedef struct edb_job_st edb_job_t;
 
 // input structures
 
@@ -157,9 +158,104 @@ enum edb_err {
  *
  ********************************************************************/
 
-// edb_open will create the file if not exists.
-#define EDB_OCREAT O_CREAT
+// if the file does not exist then create a new database.
+#define EDB_HCREAT O_CREAT
 
+typedef struct edb_host_st {
+
+	// The host will manage memeory that will be shared between
+	// handles known as the job buffer. All functions ending in
+	// -write, -load, and -free as well as edb_query and edb_datnext
+	// interact with this buffer. Each function installs a job in the
+	// buffer and that job will stay there until a worker goes through
+	// and completes that job. If the job buffer fills up, subseqent
+	// calls that interact with this buffer will start blocking.
+	//
+	// The optimial job_buffersize is related to the worker_poolsize,
+	// the speed of individual cores of the hardware, and the freqency
+	// of expensive operations.
+	//
+	// job_buffersize must be an multiple of the size of a job
+	// structure (sizeof(edb_job_t)). Said multiple should be equal or
+	// larger than worker_poolsize otherwise you risk workers doing
+	// nother but taking up resources.
+	//
+	// The job buffer will be completely allocated on startup.
+	// 
+	// A good heuristic here is to have the buffer size equal to the
+	// worker_poolsize squared:
+	//
+	//  job_buffersize = sizeof(edb_job_t) * worker_poolsize * worker_poolsize
+	//
+	off_t job_buffersize;
+
+	// Whilest hosting, the host will manage memory that will be
+	// shared between handles known as the event buffer. And
+	// distributed to handles via edb_select.
+	//
+	// Small buffers save memory but can result in more lost-events
+	// for slower performing handles. There's no scientific way to
+	// completely remove the chance of lost events, at least not in
+	// the functionality of this library alone. This is because the
+	// collection of events by their respective handlers is not
+	// allowed to compromise the efficiency of the database (ie. if 1
+	// handle is being very slow, edb is designed to prevent that
+	// slowness from spreading to other handles).
+	//
+	// event_buffersize must be a non-zero multiple of the size of an
+	// event (sizeof(edb_event_t)). The entirety of the event buffer
+	// is allocated upon startup and its size does not change.
+	//
+	// The proper buffer size is directly related to the amount of
+	// operations per second and the speed of the handles. A good
+	// heuristic would be 32*sizeof(edb_event_t) for new users. Once
+	// you start seeing event loss, you should first work on the
+	// efficiency of handles and then look to increasing this number.
+	// 
+	off_t event_buffersize;
+	
+	// Dictates the worker pool count that will be managed to
+	// serve queries. On paper, the optimial amount of workers is
+	// equal to the number of cores on the hardware (see
+	// get_nprocs(2)). But it is up to you to and your knowledge of
+	// your hardware to decide.
+	//
+	// worker_poolsize must be at least 1. Note that if
+	// worker_poolsize is indeed 1 this will result in no new workers
+	// to be created outside of the thread that was used to call
+	// edb_host.
+	int worker_poolsize;
+
+	// Jobs sent to the database will need to move pages to and from
+	// the underlying filesystem and memory. page_buffermax dicates
+	// the maximum amount of pages that can exist in memory. Some
+	// frequently accessed pages are also cached to improve
+	// efficiency. In short, the database will operate better the more
+	// ram you throw at it.
+	//
+	// There is a critical minimum that page_buffermax must be
+	// otherwise the database cannot be hosted. This minimum is not
+	// defined here but will be returned as an error by edb_host.
+	// 
+	// page_buffermax must be a multiple of the system's page size as
+	// returned by sysconf(_SC_PAGE_SIZE). Setting page_buffermax to 0
+	// means there is no maximum: the buffer can grow as long as there
+	// is memory available.
+	//
+	// Unlike some of the other buffers, this number dicates a maximum
+	// size, not a static size. The buffer will allocate as needed up
+	// too the maximum.
+	//
+	// Operating under a full buffer will cause jobs to slow, fail,
+	// and return early errors. So give this buffer plenty of space.
+	off_t page_buffermax;
+	
+	// See EDB_H... family of constants
+	int flags;
+	
+} edb_host_t;
+
+// edb_open will create the file if not exists.
 typedef struct edb_open_st {
 
 	// the file path
@@ -168,20 +264,41 @@ typedef struct edb_open_st {
 	// the agent id (arbitrary, more of a cookie)
 	uint64_t agent;
 
-	// see EDB_O... options
-	int openoptions;
+	// 
+	int flags;
 	
 } edb_open_t;
+
+// edb_host will start hosting a database for the given regular file
+// at path. edb_host will write-lock the file and will abduct the
+// calling thread and will only return once edb_hoststop is executed.
+//
+// See the edb_host_t structure.
+//
+// A successful shutdown invoked by edb_hoststop will have both
+// edb_host and edb_hoststop return without error.
+//
+// errors:
+//   EDB_EINVAL - wpoolc is 0.
+//   EDB_EOPEN  - annother process already has the file open.
+//   EDB_EFILE  - path is not a regular file.
+//   EDB_EHW    - this file was created on a different (non compatible) architecture
+//   EDB_ENOTVALID - file is invalid format, possibliy not a database.
+//
+// Thread Safety: both edb_hoststop and edb_host is thread safe. The
+// aformentioned write-locks use Open File Descriptors (see fcntl(2)),
+// this means that attempts to open the same file in two seperate
+// threads will behave the same way as doing the same with two
+// seperate processes.
+// 
+edb_err edb_host(const char *path, edb_host_t hostops);
+edb_err edb_hoststop(const char *path);
 
 // edb_open errors:
 //   EDB_EINVAL - handle is null.
 //   EDB_EINVAL - params.path is null
 //   EDB_EERRNO - error with stat(2), open(2), (ie, file does not exist, permssions)
-//   EDB_EFILE  - path is not a regular file.
-//   EDB_ENOTVALID - file is invalid format, possibliy not a database.
-//   EDB_EHW    - this file was created on a different (non compatible) architecture
-//   EDB_EOPEN  - annother process already has the file open.
-//   EDB_ENOHOST - no host process found and cannot be started
+//   EDB_ENOHOST - file is not being hosted (see edb_host)
 //
 // These functions provide access to a database provided by the
 // instrunctions set forth in params. edb_open will write to edbh and
@@ -350,6 +467,13 @@ edb_err edb_datnext (edbh *handle, edb_data_t *data);
  * Query functions
  *
  ********************************************************************/
+
+typedef struct edb_event_st {
+} edb_event_t;
+
+
+typedef struct edb_select_st {
+} edb_select_t;
 
 // Select
 //
