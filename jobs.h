9bbb05b7454e85e6337ee9f2912ddd8edf8ab5db
Kevin Marschke <kmarschke@ellem.ai>
2022-09-18 06:19:13 +0200

    started to b-line execjob_objcopy.
    Got a draft done, but see comment i made.

--- jobs.h
+++ jobs.h
@@ -14,6 +14,7 @@ typedef enum _edb_jobclass {
 	// structure ops
 	EDB_STRUCT = 0x0001,
 
+	// dynamic data ops
 	// valuint64 - the objectid (0 for new)
 	// valuint   - the length of that data that is to be written.
 	// valbuff   - the name of the shared memory open via shm_open(3). this
@@ -21,6 +22,7 @@ typedef enum _edb_jobclass {
 	//             of the object. Can be null for deletion.
 	EDB_DYN = 0x0002,
 
+	// object ops
 	// valuint64 - the objectid (cannot be 0)
 	// valuint   - the amount of bytes to copy over. If this is zero, then it will
 	//             be set to total
@@ -28,8 +30,28 @@ typedef enum _edb_jobclass {
 	//             be opened as write. Leave null to just
 	EDB_OBJ = 0x0003,
 
+	// locks for objects and structures.
+	// todo: move these into EDB_OBJ and EDB_STRUCT?
+	EDB_LCK = 0x0004
+
 } edb_jobclass;
 
+// job slot structures are met to be stored in shared memory
+// and be accessed by multiple processes.
+//
+// each job has a buffer known as a 'transferbuffer' which is
+// a set amount of bytes (allocated also in shared memory) that
+// the worker and the handle can use to send information back
+// and fourth.
+//
+// Note that there is no pointer to that particular buffer here.
+// Infact, there's no pointers at all within this structure.
+// This is because we're talking about multiple processes accessing
+// shared memeory... a pointer's value in one process can
+// mean something else entirely in another process. Even if they
+// eventually point to the same spot in physical memory.
+//
+// So we must stick to offsets for guidance (transferbuffoff)
 typedef struct edb_job_st {
 
 	// Job desc is a xor'd value between 1 edb_jobclass, 1 edb_cmd.
@@ -45,7 +67,7 @@ typedef struct edb_job_st {
 	uint32_t          futex_transferbuffc; // the amount of the buffer that is full.
 	unsigned int      writehead; // data ends at this index
 	unsigned int      readhead;  // data starts at this index
-	int state; // 0 means closed, 1 means open.
+	int state;                   // 0 means closed, 1 means open.
 	pthread_mutex_t   bufmutex;  // multiprocess mutex.
 
 	// used by the worker pool.
@@ -58,15 +80,18 @@ typedef struct edb_job_st {
 	unsigned long int jobid;
 
 	// the use of data depends on the class and command.
-	edb_data_t data;
+	//edb_data_t data;
 
 } edb_job_t;
 
 // analogous to read(2) and write(2).
 // Write will block if the buffer becomes full.
 // Read will block if the buffer becomes empty.
-// They both return the total amount of bytes read/written or -1 on critical error (which shouldn't ever happen),
-// and -2 on EOF
+// They both return the total amount of bytes read/written or -1 on critical error (which shouldn't ever happen) (will be logged),
+// and -2 on EOF.
+//
+// todo: confim the next statement:
+// the returned amount of bytes read will always be equal to count if non-error.
 //
 // edb_jobclose and edb_jobreset simply change the state of the buffer. edb_jobclose will cause further reads
 // and writes to return -2 until edb_jobreset is called.
@@ -74,7 +99,7 @@ typedef struct edb_job_st {
 // edb_jobreset is also sufficient in reseting the job's buffer.
 //
 //
-// transferbuf should be equal to shm->transbuffer.
+// transferbuf should be equal to shm->transbuffer (found in the host/workers)
 //
 // TREADING
 //   only 1 thread/process can call edb_jobread and another can call
