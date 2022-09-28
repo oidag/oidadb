f3492e480c7ab94b729b68985e9cf7fe2fa4601a
Kevin Marschke <kmarschke@ellem.ai>
2022-09-28 18:57:40 +0200

    BROKEN: some work to edbl. current structs are not good enough. see hmmmm's.

--- c.src/edbw.c
+++ c.src/edbw.c
@@ -83,11 +83,11 @@ void static execjob_objcopy(edb_worker_t *self, edb_job_t *job) {
 
 	// perform the lookup to get the entry data
 	edb_entry_t entrydat;
-	edbl_object(&self->lockdir, search, EDBL_SHARED); // todo: check for errors? // todo: make sure its unlocked
+	edbl_lobject(&self->lockdir, search, EDBL_SHARED); // todo: check for errors? // todo: make sure its unlocked
 	err = edbs_index(self->shm, entryid, &entrydat);
 	if(err) {
 		log_errorf("the supplied edb_oid does not have a valid entryid: %d", entryid);
-		edbl_object(&self->lockdir, search, EDBL_NONE);
+		edbl_lobject(&self->lockdir, search, EDBL_UNLOCK);
 		err = EDB_ENOENT;
 		edb_jobwrite(job, transbuf, &err, sizeof(err));
 		return;
@@ -99,7 +99,7 @@ void static execjob_objcopy(edb_worker_t *self, edb_job_t *job) {
 		// the page offset is larger than the amount of pages we have.
 		// thus, this rowid is impossible.
 		log_errorf("the supplied edb_oid has a page offset that is too large: %ld", pageoffset);
-		edbl_object(&self->lockdir, search, EDBL_NONE);
+		edbl_lobject(&self->lockdir, search, EDBL_UNLOCK);
 		err = EDB_ENOENT;
 		edb_jobwrite(job, transbuf, &err, sizeof(err));
 		return;
@@ -118,7 +118,7 @@ void static execjob_objcopy(edb_worker_t *self, edb_job_t *job) {
 	                 pageoffset,
 	                 &foundpage);
 	if(err) {
-		edbl_object(&self->lockdir, search, EDBL_NONE);
+		edbl_lobject(&self->lockdir, search, EDBL_UNLOCK);
 		edb_jobwrite(job, transbuf, &err, sizeof(err));
 		return;
 	}
@@ -132,7 +132,7 @@ void static execjob_objcopy(edb_worker_t *self, edb_job_t *job) {
 	err = edbp_start(&self->edbphandle, &foundpage);
 	if(err) {
 		log_critf("unhandled error %d", err);
-		edbl_object(&self->lockdir, search, EDBL_NONE);
+		edbl_lobject(&self->lockdir, search, EDBL_UNLOCK);
 		edb_jobwrite(job, transbuf, &err, sizeof(err));
 		return;
 	}
@@ -175,7 +175,7 @@ void static execjob_objcopy(edb_worker_t *self, edb_job_t *job) {
 
 	// unlock the object from the lockdir.
 	unlockobj:
-	edbl_object(&self->lockdir, search, EDBL_NONE);
+	edbl_lobject(&self->lockdir, search, EDBL_UNLOCK);
 }
 
 // helper func to selectjob.
---
+++ c.src/options.h
@@ -0,0 +1,8 @@
+#ifndef _edf_options_
+#define _edf_options_ 1
+
+// Adds more to structures and functions that
+// make sure everything operates properly.
+#define EDBL_DEBUG
+
+#endif
