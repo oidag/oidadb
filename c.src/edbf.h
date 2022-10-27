20292088c4dc97dc3760ab36b71e1e68e0250747
kevin marschke <kevin-fake-email@oidag.com>
2022-10-27 19:30:44 +0200

    Fractured parts of edbw into edbf. This was a long time coming.
    Nothing has been fully refactored, but my goal is to have edbw /not/ see edbp nor edbl but instead focus those jobs exclusively in edbf.h
    
    Also.. I don't like 'edbp_types'.... something about it idk.

---
+++ c.src/edbf.h
@@ -0,0 +1,10 @@
+
+
+#include "edbl.h"
+#include "edbp.h"
+#include "file.h"
+
+// EDBF - File operations' manager including the loading, paging, caching
+// and locking mechanisms of performing operations on an edb file.
+//
+//
