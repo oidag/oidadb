d77090f0d37f66474b67bfb8ea58c627ac072a56
kevin marschke <kevin-fake-email@oidag.com>
2022-12-12 13:46:15 -0600

    analytics.h: finally added some functions

--- c.src/analytics.h
+++ c.src/analytics.h
@@ -1,5 +1,9 @@
-
+#include "include/ellemdb.h"
 
 
 // todo: analytics_pagefault(), analytics_trashfault(), ect... stuff for dashboard
 // nothing should error.
+
+// called when new lookup/object pages are successfully created AND referenced.
+void analytics_newlookuppages(unsigned int entryid, edb_pid pid);
+void analytics_newobjectpages(unsigned int entryid, edb_pid startpid, int straitc);
