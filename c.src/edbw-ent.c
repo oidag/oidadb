be422b819b0cbc83b9cff51f2087c02edf90871f
kevin marschke <kevin-fake-email@oidag.com>
2022-12-23 09:37:15 -0600

    edba/edbw: got entry creation/deletion implemented save for page-trash cycel.
    See todo, I have to jump to ODB-57 to finish this off.

--- c.src/edbw-ent.c
+++ c.src/edbw-ent.c
@@ -11,12 +11,32 @@ edb_err edbw_u_entjob(edb_worker_t *self) {
 
 	// get params
 	edb_entry_t entryparams;
+	edb_eid eid;
 	switch (jobdesc & 0xFF00) {
 		case EDB_CCREATE:
 			edbs_jobread(job, &entryparams, sizeof(entryparams));
+			err = edba_entryopenc(handle, &eid, EDBA_FCREATE);
+			if(err) {
+				edbs_jobwrite(job, &err, sizeof(err));
+				return err;
+			}
+			err = edba_entryset(handle, entryparams);
+			if(err) {
+				edba_entryclose(handle);
+				edbs_jobwrite(job, &err, sizeof(err));
+				return err;
+			}
+			edbs_jobwrite(job, &err, sizeof(err));
+			edbs_jobwrite(job, &eid, sizeof(eid));
+			edba_entryclose(handle);
+			return 0;
 		case EDB_CDEL:
+			edbs_jobread(job, &eid, sizeof(eid));
+			err = edba_entrydelete(handle, eid);
+			edbs_jobwrite(job, &err, sizeof(err));
+			return err;
 		default:
 			return EDB_EJOBDESC;
 	}
-	return 0;
+	return err;
 }
