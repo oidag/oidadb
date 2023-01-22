5e736754a53dc1f53897664338fda6b565c5bd04
Kevin Marschke <kmarschke@ellem.ai>
2023-01-22 15:37:37 -0600

    thetool: logging function

--- c.src/tests/stupididea/error.h
+++ c.src/tests/stupididea/error.h
@@ -1,6 +1,7 @@
 #ifndef error_h_
 #define error_h_
 
-void error(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+void log_error(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 
+void log_verbose(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 #endif
