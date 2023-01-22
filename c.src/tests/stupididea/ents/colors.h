2886b7d7122cc5bf5b58c377b968157cfb502e3f
Kevin Marschke <kmarschke@ellem.ai>
2023-01-22 17:52:14 -0600

    thetool: more colors

--- c.src/tests/stupididea/ents/colors.h
+++ c.src/tests/stupididea/ents/colors.h
@@ -7,7 +7,7 @@
 typedef vec3ub color_t;
 
 #define _COLOR(color,shade,hexcode) const static color_t color_##color##shade = { \
-		(0x##hexcode >> 0x12) & 0xFF,\
+		(0x##hexcode >> 0x10) & 0xFF,\
 		(0x##hexcode >> 0x8) & 0xFF,\
 		0x##hexcode & 0xFF,\
 };
@@ -15,6 +15,9 @@ typedef vec3ub color_t;
 // cyans
 _COLOR(cyan, 50, ecfeff)
 _COLOR(cyan, 100, cffafe)
+_COLOR(cyan, 200, a5f3fc)
+_COLOR(cyan, 300, 67e8f9)
+_COLOR(cyan, 400, 22d3ee)
 _COLOR(cyan, 500, 06b6d4)
 _COLOR(cyan, 600, 0284c7)
 _COLOR(cyan, 700, 0369a1)
@@ -22,6 +25,16 @@ _COLOR(cyan, 800, 155e75)
 _COLOR(cyan, 900, 164e63)
 
 // pinks
+_COLOR(pink, 50, fff1f2)
+_COLOR(pink, 100, ffe4e6)
+_COLOR(pink, 200, fecdd3)
+_COLOR(pink, 300, fda4af)
+_COLOR(pink, 400, fb7185)
+_COLOR(pink, 500, f43f5e)
+_COLOR(pink, 600, e11d48)
+_COLOR(pink, 700, be123c)
+_COLOR(pink, 800, 9f1239)
+_COLOR(pink, 900, 881337)
 
 // emeralds
 _COLOR(emerald, 50, ecfdf5)
@@ -51,6 +64,13 @@ _COLOR(slate, 900, 0f172a)
 _COLOR(stone, 700, 44403c)
 _COLOR(stone, 800, 292524)
 _COLOR(stone, 900, 1c1917)
+
+
+// violet
+_COLOR(violet, 300, c4b5fd)
+_COLOR(violet, 400, a78bfa)
+_COLOR(violet, 800, 5b21b6)
+_COLOR(violet, 900, 4c1d95)
 
 void static inline color_glset(color_t c) {
 	glColor3ubv((GLubyte*)&c);
