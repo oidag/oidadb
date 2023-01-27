d738ad9e3fb07d5edacbdcecc4bd8be6c69814f5
Kevin Marschke <kmarschke@ellem.ai>
2023-01-27 01:22:49 -0600

    testing: more meta-programming and spec-ing

--- c.src/tests/t0001_io.c
+++ c.src/tests/t0001_io.c
@@ -1,5 +1,22 @@
 
+#include <stdio.h>
 #include "teststuff.h"
 #include "../include/oidadb.h"
 
+void t0001() {
+	edb_hostconfig_t hostops = {0};
 
+	err = odb_host(test_filenmae, hostops);
+	if(err) {
+		test_error("failed to host");
+		return;
+	}
+
+}
+
+int main(int argc, const char **argv) {
+	test_mkdir();
+	test_mkfile(argv[0]);
+	t0001();
+	return test_waserror;
+}
