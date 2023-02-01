f2eaae3abe16d5fffb88994ba7041936ef01d483
kevin marschke <kevin-fake-email@oidag.com>
2023-01-31 21:24:55 -0600

    testing: wrote t0005, now got to go define everything.

--- c.src/tests/t0005_host.c
+++ c.src/tests/t0005_host.c
@@ -1,30 +1,57 @@
-#include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
+#include "teststuff.h"
 #include "../include/oidadb.h"
 #include "../edbp.h"
 #include "../edbd.h"
-#include <pthread.h>
 #include "../edbh.h"
 
-int main() {
-	unlink(".tests/hosttest");
-	edb_hostconfig_t config = {
-			.worker_poolsize = 8,
-			.event_buffersize = 16,
-			.flags = EDB_HCREAT,
-			.page_multiplier = 2,
-			.maxstructurepages = 1,
-			.job_buffersize = 16,
-			.job_transfersize = 4096,
-			.slot_count = 32
-	};
-	edb_err err = 0;
-	err = edb_host(".tests/hosttest", config);
-	if(err) goto end;
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-	end:
-	if(err)
-		printf("failed with error: %d\n", err);
-	return err;
+static void *closeme(void *_) {
+
+	int i;
+	for( i = 0; i < 10; i++) {
+		err = odb_hoststop(test_filenmae);
+		if(err) {
+			if(err != EDB_ENOHOST)
+			test_error("odb_hoststop returned non-0 that wasn't EDB_ENOHOST");
+			return 0;
+		}
+		usleep(500);
+	}
+	if(i == 10) {
+		test_error("odb_hoststop tried 10 times to close host but the host "
+				   "was never found");
+	}
+	return 0;
+}
+
+int t0005() {
+	err = odb_create(test_filenmae, odb_createparams_defaults);
+	if(err) {
+		test_error("failed to create file");
+		return test_waserror;
+	}
+
+	// start a pthread
+	pthread_t thread;
+	pthread_create(&thread, 0, closeme, 0);
+
+	err = odb_host(test_filenmae, odb_hostconfig_default);
+	if(err) {
+		test_error("host returned non-0");
+		return 1;
+	}
+
+
+	return test_waserror;
+}
+
+int main(int argc, const char **argv) {
+	test_mkdir();
+	test_mkfile(argv[0]);
+	t0005();
+	return test_waserror;
 }
