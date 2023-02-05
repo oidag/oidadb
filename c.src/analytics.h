573a37e74c2c77b971ce13168186a64155a9e8d6
kevin marschke <kevin-fake-email@oidag.com>
2023-02-04 23:01:45 -0600

    hotfix

--- c.src/analytics.h
+++
@@ -1,8 +0,0 @@
-#include "include/oidadb.h"
-
-
-// todo: analytics_pagefault(), analytics_trashfault(), ect... stuff for dashboard
-// nothing should error.
-
-// called when new lookup/object pages are successfully created AND referenced.
-void analytics_newobjectpages(unsigned int entryid, edb_pid startpid, unsigned int straitc);
