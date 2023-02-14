bdae7924c9bdddb0ae45510b97a2cdbc953bcb28
Kevin Marschke <kmarschke@ellem.ai>
2023-02-13 23:18:23 -0600

    edbd / edbl: introduced special relationship
    Sense the relationship between edbl and fcntl is a "backend only" relationship, that means I hesitated to expose any other functionaility to expose this backend only relationship.
    
    fcntl needs to have access to descriptor items which edbd has so far held dear. So I introduced edbd-edbl.c... see namespaces.org.

---
+++ c.src/edbd-edbl.c
@@ -0,0 +1,20 @@
+// this file is only met for edbl
+#include "edbd_u.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int edbl_reopen(const edbd_t *file, int flags, mode_t mode) {
+	char buff[255];
+	sprintf(buff, "/proc/%d/fd/%d", getpid(), file->descriptor);
+	return open(buff, flags, mode);
+}
+unsigned int edbl_pageoffset(const edbd_t *file, edb_eid eid) {
+	int pageoff = eid / file->enteriesperpage;
+	return edbd_size(file) // skip intro page
+	       + edbd_size(file) * pageoff // skip any pages containing previous eids
+	       + ODB_SPEC_HEADSIZE // skip the head of that page.
+		   + (eid % file->enteriesperpage *sizeof(odb_spec_index_entry));
+}
