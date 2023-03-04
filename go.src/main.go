26f272ae5b7e9582d9302c458292f30856c5344c
Kevin Marschke <kmarschke@ellem.ai>
2023-03-03 22:07:06 -0600

    odbm-go: we're live with glfw.
    Note: the licensing is freaking me out.
    
    C-to-go translation coming soon.

--- go.src/main.go
+++ go.src/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "github.com/go-gl/glfw/v3.3/glfw"
+
 func main() {
 	println("d")
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	w, err := glfw.CreateWindow(1200, 800, "test", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	w.SetPos(2560*2+100, 100)
+	w.MakeContextCurrent()
+
+	for !w.ShouldClose() {
+		w.SwapBuffers()
+		glfw.PollEvents()
+	}
+
 }
