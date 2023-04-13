1c627e99ae6147e5e6c2558123d4d6a9657da2aa
kevin marschke <kevin-fake-email@oidag.com>
2023-04-13 18:53:47 -0500

    manual: more walking down and filling everything out. Pretty much done with v0.1.0 minus editing.
    Also note clange.org

--- c.src/include/oidadb.h
+++ c.src/include/oidadb.h
@@ -120,7 +120,10 @@ typedef enum odb_err {
 	/// Something was missed
 	ODB_EMISSED,
 
-	ODB_EDELETED
+	ODB_EDELETED,
+
+	/// Something wrong with buffer size.
+	ODB_EBUFFSIZE,
 } odb_err;
 export const char *odb_errstr(odb_err error);
 
@@ -198,141 +201,23 @@ typedef struct odb_entstat_t {
 } odb_entstat_t;
 export odb_err odbh_index(odbh *handle, odb_eid eid, odb_entstat_t *o_entry);
 
-/** \brief Get structure data
- *
- * Write index information into `o_entry` with the given `eid`.
- *
- * If a certain entry is undergoing an exclusive-lock job, the function call
- * is blocked until that operation is complete.
- *
- * ## VOLATILITY
- *
- * When this function returns, `o_struct` may already be out of date if
- * another operation managed to update the structure contents.
- *
- * ## ERRORS
- *
- *   ODB_EEOF - sid was too high (out of bounds)
- *
- * \see elements to for information as to what a structure is.
- */
-export odb_err odbh_structs(odbh *handle, odb_sid structureid, void *o_struct)
-; //
-// todo: what is o_struct?
+typedef struct odb_structstat_t {
+	unsigned int fixedc;
+	unsigned int dynmc;
+	unsigned int confc;
+	void *confv;
+} odb_structstat_t;
+export odb_err odbh_structs(odbh *handle
+							, odb_sid structureid
+							, odb_structstat_t *o_struct);
 
-/**
- * \brief Command enumeration
- *
- * This enum alone doesn't describe much. It depends on the context of which
- * odbh function is being used.
- */
-typedef enum odb_cmd {
-	ODB_CNONE   = 0x0000,
-	ODB_CREAD   = 0x0100,
-	ODB_CWRITE  = 0x0200,
-	ODB_CCREATE = 0x0300,
-	ODB_CDEL    = 0x0400,
-	ODB_CUSRLKR  = 0x0500,
-	ODB_CUSRLKW  = 0x0600,
-	ODB_CSELECT  = 0x0700,
-} odb_cmd;
+typedef enum odb_option_t {
+	ODB_OFILTER,
+	ODB_OCOOKIE,
+} odb_option_t;
+export odb_err odbh_tune(odbh *handle, odb_option_t option, ... /* args */);
 
-// odb_jobclass must take only the first 4 bits. (to be xor'd with
-// edb_cmd).
-typedef enum odb_jobclass {
-
-	// means that whatever job was there is now complete and ready
-	// for a handle to come in and install another job.
-	EDB_JNONE = 0x0000,
-
-	// structure ops
-	//
-	//   (ODB_CWRITE: not supported)
-	//   ODB_CCREATE:
-	//     <- edb_struct_t (no implicit fields)
-	//     -> odb_err (parsing error)
-	//     <- arbitrary configuration
-	//     -> uint16_t new structid
-	//   ODB_CDEL:
-	//     <- uint16_t structureid
-	//     -> odb_err
-	EDB_STRUCT = ODB_ELMSTRCT,
-
-	// dynamic data ops
-	// valuint64 - the objectid (0 for new)
-	// valuint   - the length of that data that is to be written.
-	// valbuff   - the name of the shared memory open via shm_open(3). this
-	//             will be open as read only and will contain the content
-	//             of the object. Can be null for deletion.
-	EDB_DYN = ODB_ELMDYN,
-
-	// Perform CRUD operations with a single object.
-	// see edb_obj() for description
-	//
-	// all cases:
-	//     <- odb_oid (see also: EDB_OID_... constants)
-	//     (additional params, if applicable)
-	//     -> odb_err [1]
-	//  ODB_CREAD:
-	//     (all cases)
-	//     -> void *rowdata
-	//     ==
-	//  ODB_CWRITE:
-	//     (all cases)
-	//     <- void *rowdata
-	//     ==
-	//  ODB_CCREATE:
-	//     (all cases)
-	//     // todo: what if they think the structure is X bytes long and sense has been updated and thus now is X+/-100 bytes long?
-	//     <- void *rowdata (note to self: keep this here, might as well sense we already did the lookup)
-	//     -> created ID
-	//     ==
-	//  ODB_CDEL
-	//     (all cases)
-	//     ==
-	//  EDB_CUSRLK(R/W): (R)ead or (W)rite the persistant locks on rows.
-	//     (all cases)
-	//     (R) -> edb_usrlk
-	//     (W) <- edb_usrlk
-	//     ==
-	//
-	// [1] This error will describe the efforts of locating the oid
-	//     which will include:
-	//       - ODB_EHANDLE - handle closed stream/stream is invalid
-	//       - ODB_EINVAL - entry in oid was below 4.
-	//       - ODB_EINVAL - jobdesc was invalid
-	//       - ODB_EINVAL - (ODB_CWRITE) start wasn't less than end.
-	//       - ODB_ENOENT - (ODB_CWRITE, ODB_CREAD) oid is deleted todo:
-	//        todo: change this to entity not valid
-	//       - ODB_EOUTBOUNDS - (ODB_CWRITE): start was higher than fixedlen.
-	//       - ODB_EULOCK - failed due to user lock (see EDB_FUSR... constants)
-	//       - ODB_EEXIST - (ODB_CCREATE): Object already exists
-	//       - ODB_ECRIT - unknown error
-	//       - ODB_ENOSPACE - (ODB_CCREATE, using AUTOID): disk/file full.
-	//       - ODB_EEOF - the oid's entry or row was larger than the most possible value
-	//
-	EDB_OBJ = ODB_ELMOBJ,
-
-	// Modify the entries, updating the index itself.
-	//
-	// EDB_ENT | ODB_CCREATE
-	//   <- edb_entry_t entry. Only the "parameters" group of the structure is used.
-	//      This determains the type and other parameters.
-	//   -> odb_err error
-	//   -> (if no error) uint16_t entryid of created ID
-	//
-	// EDB_ENT | ODB_CDEL
-	//   <- uint16_t entryid of ID that is to be deleted
-	//   -> odb_err error
-	//
-	EDB_ENT = ODB_ELMENTS,
-
-} odb_jobclass;
-
-
-// OR'd together values from odb_jobclass and odb_cmd
-typedef enum odb_jobdesc {
-
+typedef enum odb_jobtype_t {
 	// Objects
 	ODB_JCREATE
 	, ODB_JDELETE
@@ -346,11 +231,25 @@ typedef enum odb_jobdesc {
 	, ODB_JSTRCTDELETE
 
 	// Entities
+	, ODB_JENTCREATE
+	, ODB_JENTDELETE
 
 	// Dynamics
+} odb_jobtype_t;
+export odb_err odbh_job   (odbh *handle, odb_jobtype_t jobtype);
+export odb_err odbh_jwrite(odbh *handle, const void *buf, int bufc);
+export odb_err odbh_jread (odbh *handle, void *o_buf, int bufc);
+export odb_err odbh_jclose(odbh *handle);
 
-} odb_jobdesc;
+typedef enum odb_eventtype_t {
+	ODB_VWRITE,
+	ODB_VCREATE,
+	ODB_VDELETE,
+} odb_eventtype_t;
+typedef struct odb_event_t {
 
+} odb_event_t;
+export odb_err odbh_poll(odbh *handle, odb_event_t *o_evt);
 
 /**
  * \brief User lock bit constants
@@ -377,76 +276,5 @@ typedef enum odb_usrlk {
 
 	_EDB_FUSRLALL = 0x000F,
 } odb_usrlk;
-
-
-
-// todo: I would like to this function to always return immediately. Need to
-//  find a clever way of doing returns. Maybe like a "poll return"... perhaps
-//  you can submite 40 jobs all at once and then poll all the returns?
-/***
-\brief Install a job.
-
-Here be the most important function of this entire library: the ability to
-install a job. The host maintains a buffer of jobs and workers in the host
-will scan that buffer and execute said jobs.
-
-This function's signature is analogous to `fcntl(2)`: in which the first
-argument (`handle`) is the handle, second argument (`jobclass`) is the
-'command' and depending on the command dictates `args`.
-
- - The job buffer is full and thus this function must wait until
-   other jobs complete for a chance of getting into the buffer.
-
-`jobclass` is an XOR'd together integer with the following combinations:
-
-## `ODB_ELMOBJ | ODB_CREAD` (odb_oid id, voido_bufv, int bufc, int offset)
-    Read the contents of the object with `id` into `o_bufv` up to `bufc`
-    bytes.
-    todo: see above comment
-
-## `ODB_ELMOBJ | ODB_CWRITE` (odb_oid id, voidbufv, int bufc, int offset)
-    Write bytes stored at `bufv` up to `bufc` into the object with `id`.
-
-## `ODB_ELMOBJ | ODB_CCREATE` (odb_oid *o_id, void *bufv, int bufc, int offset)
-   Create a new object
-
-   During creation, the new object is written starting at `offset` and
-   writes `bufc` bytes and all other bytes are initializes as 0.
-
-
-## EDB_CUSRLK
-   Install a persisting user lock. These locks will affect future calls to
-   odbh_obj
-
-\param jobclass This is a XOR'd value between one \ref odb_type and one \ref
-                odb_cmd
-
-## ERRORS
-
- - ODB_EINVAL - handle is null or uninitialized
- - ODB_EJOBDESC - odb_jobdesc is not valid
- - ODB_ECLOSED - Host is closed or is in the process of closing.
- - \ref ODB_ECRIT
-
-## VOLATILITY
-Fuck.
-
-
-
-\see odbh_structs
- */
-
-// todo: replace this with what is documented.
-typedef enum odb_jobhint_t {
-	a
-} odb_jobhint_t;
-export odb_err odbh_jobmode(odbh *handle, odb_jobhint_t hint);
-
-// todo:
-typedef uint64_t odb_jobtype_t;
-export odb_err odbh_job   (odbh *handle, odb_jobtype_t jobtype);
-export odb_err odbh_jwrite(odbh *handle, const void *buf, int bufc);
-export odb_err odbh_jread (odbh *handle, void *o_buf, int bufc);
-export odb_err odbh_jclose(odbh *handle);
 	
 #endif // _EDB_H_
