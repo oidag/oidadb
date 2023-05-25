81852c82672db578fe920352497b7a8a4b639307
Kevin Marschke <kmarschke@ellem.ai>
2023-05-24 22:16:32 -0500

    edbh-dyn: defined odbh_jdyn functions (for verison 1).
    Yup. Low hanging fruit sense we don't need dynamic functiosn working on first release.

---
+++ c.src/edbh-dyn.c
@@ -0,0 +1,27 @@
+#include "include/oidadb.h"
+struct odbh_jobret odbh_jdyn_read(odbh *handle
+		, odb_oid oid
+		, int idx
+		, void *datv
+		, int datc) {
+
+	struct odbh_jobret ret;
+	ret.err = ODB_EVERSION;
+	return ret;
+}
+struct odbh_jobret odbh_jdyn_write(odbh *handle
+		, odb_oid oid
+		, int idx
+		, const void *datv
+		, int datc) {
+	struct odbh_jobret ret;
+	ret.err = ODB_EVERSION;
+	return ret;
+}
+struct odbh_jobret odbh_jdyn_free(odbh *handle
+		, odb_oid oid
+		, int idx) {
+	struct odbh_jobret ret;
+	ret.err = ODB_EVERSION;
+	return ret;
+}
