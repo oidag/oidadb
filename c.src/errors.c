a4091e89b73a0b47dc8786a858e48dc58b800674
kevin marschke <kevin-fake-email@oidag.com>
2023-06-22 21:49:23 -0500

    defined odb_errstr
    simple macros.

--- c.src/errors.c
+++ c.src/errors.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <errno.h>
 
+#include "include/oidadb.h"
 #include "errors.h"
 
 #define stdlogthing(stream,prefix) { \
@@ -64,4 +65,39 @@ void implementme() {
 	// cause a segfaut and div0.
 	int *throw = (void *)(0);
 	*throw = 1;
+}
+
+const char *odb_errstr(odb_err error) {
+	switch (error) {
+		case ODB_ENONE:     return "ODB_ENONE";
+		case ODB_ECRIT:     return "ODB_ECRIT";
+		case ODB_EINVAL:    return "ODB_EINVAL";
+		case ODB_ENOHANDLE: return "ODB_ENOHANDLE";
+		case ODB_EHANDLE:   return "ODB_EHANDLE";
+		case ODB_ENOENT:    return "ODB_ENOENT";
+		case ODB_EEXIST:    return "ODB_EEXIST";
+		case ODB_EEOF:      return "ODB_EEOF";
+		case ODB_EFILE:     return "ODB_EFILE";
+		case ODB_ENOTDB:    return "ODB_ENOTDB";
+		case ODB_EOPEN:     return "ODB_EOPEN";
+		case ODB_ECLOSED:   return "ODB_ECLOSED";
+		case ODB_ENOHOST:   return "ODB_ENOHOST";
+		case ODB_EOUTBOUNDS:return "ODB_EOUTBOUNDS";
+		case ODB_EERRNO:    return "ODB_EERRNO";
+		case ODB_EHW:       return "ODB_EHW";
+		case ODB_ENOMEM:    return "ODB_ENOMEM";
+		case ODB_ENOSPACE:  return "ODB_ENOSPACE";
+		case ODB_ESTOPPING: return "ODB_ESTOPPING";
+		case ODB_EAGAIN:    return "ODB_EAGAIN";
+		case ODB_EULOCK:    return "ODB_EULOCK";
+		case ODB_EJOBDESC:  return "ODB_EJOBDESC";
+		case ODB_EVERSION:  return "ODB_EVERSION";
+		case ODB_EPROTO:    return "ODB_EPROTO";
+		case ODB_EBADE:     return "ODB_EBADE";
+		case ODB_EPIPE:     return "ODB_EPIPE";
+		case ODB_EMISSED:   return "ODB_EMISSED";
+		case ODB_EDELETED:  return "ODB_EDELETED";
+		case ODB_EBUFFSIZE: return "ODB_EBUFFSIZE";
+		default: return "UNDOCUMENTEDERROR";
+	}
 }
